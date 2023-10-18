2e6fbce47454f4852d99114a88a7f9ddd422cb3b
Ayush <ayush0999aryan@gmail.com>
2023-10-18 21:38:28 +0530

    18th of oct

---
+++ Js-learning/Programs/practice6.js
@@ -0,0 +1,11 @@
+// var, let, const
+
+var a = 12;
+
+let aayush = 12;
+
+// we have to write window in console 
+// coz window object give us that power 
+
+// var window pe automatically aa jata hai 
+// let window pe automatically nahi aata hai 
---
+++ Js-learning/Programs/practice7.js
@@ -0,0 +1,14 @@
+// Truthy vs Falsy
+
+// flasy = 0, flase, null, NaN, undefined, document.all 
+// truthy = anything that doesn't belong to falsy
+
+
+
+
+if (NaN){
+    console.log("Hey");
+}
+else{
+    console.log("Hello");
+}
