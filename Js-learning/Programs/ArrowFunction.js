09141cdad92f61e234382e41dceae0f9b9b12329
Ayush <ayush0999aryan@gmail.com>
2023-12-05 23:34:22 +0530

    arrow function added

---
+++ Js-learning/Programs/ArrowFunction.js
@@ -0,0 +1,21 @@
+
+
+// function sum (a , b){
+//     return a + b;
+// }
+
+// let a = 15;
+// let b = 15;
+
+// pqwe = sum(15,10);
+
+// console.log(pqwe)
+
+
+sum = (a,b) => {
+    return a + b;
+}
+
+let a = 40;
+let b = 40;
+console.log(sum(a,b));
