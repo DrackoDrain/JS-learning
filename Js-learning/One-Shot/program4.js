a88806ffe3cc2fc3c00ad6f1416391e7cac8af6b
Ayush <ayush0999aryan@gmail.com>
2024-01-10 11:49:33 +0530

    string added

---
+++ Js-learning/One-Shot/program4.js
@@ -0,0 +1,28 @@
+function countVowel(str){
+    let count = 0;
+
+    for(let i = 0 ; i < str.length; i ++ ){
+        if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+        {
+            count ++;
+        }
+    }
+    console.log(count);
+}
+
+countVowel("ayusharyan");
+
+
+function numOfVowel(str1){
+    let count = 0;
+    for (const char of str1){
+        if ( char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u'){
+            count ++;
+        }
+
+
+    }
+    console.log(count);
+}
+
+numOfVowel("bittukumar")
