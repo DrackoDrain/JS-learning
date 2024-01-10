5efcee0d918bfa45243f4985b5e661d48ce48dae
Ayush <ayush0999aryan@gmail.com>
2024-01-10 19:07:01 +0530

    strings

--- Js-learning/One-Shot/program4.js
+++ Js-learning/One-Shot/program4.js
@@ -19,8 +19,6 @@ function numOfVowel(str1){
         if ( char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u'){
             count ++;
         }
-
-
     }
     console.log(count);
 }
