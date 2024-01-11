69272227f82f6b122eea47e47a6e364f1e9a20aa
Ayush <ayush0999aryan@gmail.com>
2024-01-11 10:47:15 +0530

    map-fliter

---
+++ Js-learning/One-Shot/program6.js
@@ -0,0 +1,10 @@
+// Map 
+
+
+let arr = [12,23,34];
+
+let newArr = arr.map((val) => {
+    return val+1;
+})
+
+console.log(newArr);
---
+++ Js-learning/One-Shot/program7.js
@@ -0,0 +1,9 @@
+// Filter 
+
+let arr = [23,34,57,80]
+
+let newArr = arr.filter((val) => {
+    return val%2==0;
+})
+
+console.log(newArr);
