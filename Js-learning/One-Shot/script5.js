c74e2233929ed1d31cbd7092761fc74ae0f8e582
Ayush <ayush0999aryan@gmail.com>
2024-01-14 11:59:14 +0530

    Events

---
+++ Js-learning/One-Shot/script5.js
@@ -0,0 +1,14 @@
+let btn1 = document.getElementById("btn1")
+
+btn1.onclick = () => {
+    console.log("btn1 was clicked")
+}
+
+console.log("hi");
+
+
+let btn2 = document.getElementById("btn2")
+
+btn2.ondblclick = () => {
+    console.log("btn2 was clicked 2x")
+}
