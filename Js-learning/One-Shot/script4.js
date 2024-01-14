5f6ca2fe02409f70498c8fadbd90cf6d60181b3b
Ayush <ayush0999aryan@gmail.com>
2024-01-14 11:25:22 +0530

    append,prepend, etc

---
+++ Js-learning/One-Shot/script4.js
@@ -0,0 +1,7 @@
+let newBtn = document.createElement("button")
+newBtn.innerText = "click me!"
+
+newBtn.style.color = "red "
+newBtn.style.backgroundColor = "black"
+
+document.querySelector("body").prepend("newBtn")
