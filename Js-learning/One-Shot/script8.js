55fdb6551898d526ce9da07bb7a6499246d5fb36
Ayush <ayush0999aryan@gmail.com>
2024-01-17 12:18:53 +0530

    Objects

---
+++ Js-learning/One-Shot/script8.js
@@ -0,0 +1,19 @@
+let student = {
+
+    name : "Ayush",
+    marks : 98.43,
+    printMarks : function()  {
+        console.log("marks = ", this.marks);
+
+    },
+};
+
+const employee ={
+    calTax(){
+        console.log("Tax is 2 %");
+    },
+    calTax1 : function(){
+        console.log("Tax is 1.5%");
+
+    }
+}
