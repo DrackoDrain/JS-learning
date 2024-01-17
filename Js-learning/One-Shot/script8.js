109fc01b91b44308717a9f9b6f6699e3027f19a2
Ayush <ayush0999aryan@gmail.com>
2024-01-17 12:38:24 +0530

    Prototype

--- Js-learning/One-Shot/script8.js
+++ Js-learning/One-Shot/script8.js
@@ -2,6 +2,36 @@ let student = {
 
     name : "Ayush",
     marks : 98.43,
+    calTax(){
+        console.log("Tax is 3 %"); // this will win because this function is in objects prototype so this priority high.
+    },
+    printMarks : function()  {
+        console.log("marks = ", this.marks);
+
+    },
+};
+let student1 = {
+
+    name : "Ayush",
+    marks : 98.43,
+    printMarks : function()  {
+        console.log("marks = ", this.marks);
+
+    },
+};
+let student2 = {
+
+    name : "Ayush",
+    marks : 98.3,
+    printMarks : function()  {
+        console.log("marks = ", this.marks);
+
+    },
+};
+const student3 = {
+
+    name : "Ayush",
+    marks : 98.4,
     printMarks : function()  {
         console.log("marks = ", this.marks);
 
@@ -12,11 +42,23 @@ const employee ={
     calTax(){
         console.log("Tax is 2 %");
     },
-    calTax1 : function(){
-        console.log("Tax is 1.5%");
+    // calTax1 : function(){
+    //     console.log("Tax is 1.5%");
 
-    }
+    // }
 }
 
 // Prototype syntax = we can use another objects in out main objects by creating prototype like -> objects.__proto__objects1
-employee.__proto__ = student;
+
+// employee.__proto__ = student;
+
+
+student.__proto__ = employee;
+student1.__proto__ = employee;
+student2.__proto__ = employee;
+student3.__proto__ = employee;
+
+
+
+
+
