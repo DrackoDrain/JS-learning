ca82e2f4c83238eaaec62c78c8223c40521b4c2f
Ayush <ayush0999aryan@gmail.com>
2024-03-08 02:30:40 +0530

    spread overloading

--- array/spreadOperator.js
+++ array/spreadOperator.js
@@ -1,7 +1,17 @@
 // Spread Operator 
 
-let arr = [ 2,3,4];
-let arr1 = [5,6,7];
+// let arr = [ 2,3,4];
+// let arr1 = [5,6,7];
 
-let arr2 = [...arr, ...arr1];
-console.log(arr2);
+// let arr2 = [...arr, ...arr1];
+// console.log(arr2);
+
+
+
+let obj = {
+    name:"ayush",
+    company:"company xyz",
+    address:"XYZ"
+}
+
+console.log({...obj, name:"JHON"});
