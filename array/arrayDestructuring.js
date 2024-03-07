19bdaff2b3e908a9720830d931dd2b848f356bb4
Ayush <ayush0999aryan@gmail.com>
2024-03-08 02:21:08 +0530

    array Destructuring

---
+++ array/arrayDestructuring.js
@@ -0,0 +1,11 @@
+// Array Destructuring 
+// let arr = [2,3,4,5,6,7,8];
+
+// let [a,b,c,...rest] = arr;
+
+// console.log(a,b,c,rest);
+
+// Object destructuring 
+
+let {a,b,c,d} = {a:1,b:5, c:7, d: 10};
+console.log(a,b,c,d);
