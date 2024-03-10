d4d282168d981d1edcfd5542773e0595e21c7d75
Ayush <ayush0999aryan@gmail.com>
2024-03-10 23:07:24 +0530

    objects-proto

--- Js-learning/One-Shot/script8.js
+++ Js-learning/One-Shot/script8.js
@@ -48,7 +48,7 @@ const employee ={
     // }
 }
 
-// Prototype syntax = we can use another objects in out main objects by creating prototype like -> objects.__proto__objects1
+// Prototype syntax = we can use another objects in our main objects by creating prototype like -> objects.__proto__objects1
 
 // employee.__proto__ = student;
 
