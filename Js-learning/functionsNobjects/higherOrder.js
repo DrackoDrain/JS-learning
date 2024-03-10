42fc44b2500d218dac49dfbe3b538e1cadbe1528
Ayush <ayush0999aryan@gmail.com>
2024-03-10 23:19:40 +0530

    higherOrder

---
+++ Js-learning/functionsNobjects/higherOrder.js
@@ -0,0 +1,30 @@
+// Higher Order function
+
+
+// 1st type 
+// function add(a,b, cb){
+//     let result = a + b; 
+//     cb(result);
+
+// }
+
+
+
+
+// add(3,4,function(result){
+//     console.log(result);
+// });
+
+
+// 2nd type 
+
+function add(a,b, cb){
+        let result = a + b; 
+        cb(result);
+    }
+
+
+    function showResult(result){
+        console.log(result);
+    }
+    add (3,3, showResult)
